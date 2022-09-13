39d5daba1223cdd68b8e8229be230436357dbdcd
Jay A <jautus@hotmail.com>
2022-09-13 16:14:45 -0700

    fix-3 change app.get

--- server/server.js
+++ server/server.js
@@ -24,15 +24,21 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'build')));
 }
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
 });
+
 db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`=======================================================`);
-        console.log(`--- 🌍 Now listening on localhost:${PORT}! ---------------`);
+        console.log(`--- 🌍 Now listening on localhost:${ PORT }! ---------------`);
         // log where we can go to test our GQL API
-        console.log(`--- 🚀 Use GraphQL at http://localhost:${PORT}${server.graphqlPath} ---`);
+        console.log(`--- 🚀 Use GraphQL at http://localhost:${ PORT }${ server.graphqlPath } ---`);
         console.log(`=======================================================`);
     });
 });
